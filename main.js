3c9becf37921afa61aa7d1827922419690b6ebf7
BERKANT SOYTAS <berkantsoytasofficial@gmail.com>
2021-08-05 17:01:10 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,121 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu, ipcMain, ipcRenderer } = electron;
+
+let mainWindow, addWindow;
+
+app.on("ready", () => {
+  // Pencere tanımlanıp Ayarlamaları
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.setResizable(false);
+  mainWindow.loadURL(
+    url.format(
+      {
+        pathname: path.join(__dirname, "pages/main.html"),
+        protocol: "file:",
+        slashes: true,
+      },
+      { unicode: "utf-8" }
+    )
+  );
+
+  // Eğer Ana sayfa kapatılırsa tüm uygulama kapanacak!
+  mainWindow.on("close", () => {
+    app.quit();
+  });
+
+  // Menünün Oluşturulması
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  // New TODO Penceresinin Eventleri
+  ipcMain.on("newTodo:close", (err, data) => {
+    addWindow.close();
+    addWindow = null;
+  });
+});
+
+const mainMenuTemplate = [
+  {
+    label: "Dosya",
+    submenu: [
+      {
+        label: "Yeni TODO Ekle",
+        accelerator: "Ctrl+N",
+        click() {
+          createWindow();
+        },
+      },
+      {
+        label: "Tüm TODO'ları Sil",
+      },
+      {
+        label: "Çıkış",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        role: "quit",
+      },
+    ],
+  },
+];
+
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({
+    label: app.getName(),
+    role: "TODO",
+  });
+}
+
+if (process.env.NODE_ENV != "production") {
+  mainMenuTemplate.push({
+    label: "Dev Tools",
+    submenu: [
+      {
+        label: "Geliştirici Araçları",
+        click(item, focusWindow) {
+          focusWindow.toggleDevTools();
+        },
+      },
+      {
+        label: "Yenile",
+        role: "reload",
+      },
+    ],
+  });
+}
+
+function createWindow() {
+  addWindow = new BrowserWindow({
+    width: 480,
+    height: 183,
+    title: "Yeni Pencere",
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  addWindow.loadURL(
+    url.format(
+      {
+        pathname: path.join(__dirname, "pages/newTodo.html"),
+        protocol: "file:",
+        slashes: true,
+      },
+      { unicode: "utf-8" }
+    )
+  );
+
+  addWindow.setResizable(false);
+
+  addWindow.on("close", () => {
+    addWindow = null;
+  });
+}
